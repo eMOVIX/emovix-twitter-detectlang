8c38f725d347b18ad1151348d676f94be8a4cd5c
Jordi Vilaplana <gatasuna@gmail.com>
2016-02-03 11:38:04 +0100

    Updated language detection.

--- emovix-twitter-detectlang.py
+++ emovix-twitter-detectlang.py
@@ -8,12 +8,13 @@ import time
 
 logging.basicConfig(
     filename='emovix_twitter_detectlang.log',
-    level=logging.WARNING,
+    level=logging.DEBUG,
     format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
     datefmt='%d-%m-%y %H:%M')
 
 # Configuration parameters
 detectlanguage_api_key = ""
+database_host = ""
 database_name = ""
 
 client = None
@@ -26,9 +27,10 @@ if __name__ == '__main__':
     with open('config.json', 'r') as f:
         config = json.load(f)
         detectlanguage_api_key = config['detectlanguage_api_key']
+        database_host = config['database_host']
         database_name = config['database_name']
 
-    client = MongoClient('mongodb://localhost:27017/')
+    client = MongoClient('mongodb://' + database_host + ':27017/')
     db = client[database_name]
 
     detectlanguage.configuration.api_key = detectlanguage_api_key
@@ -37,16 +39,22 @@ if __name__ == '__main__':
         try:
             if detectlanguage.user_status()['requests'] >= detectlanguage.user_status()['daily_requests_limit']:
                 logging.debug("Number of requests over daily limit.")
-                time.sleep(5)
+                time.sleep(60)
 
-            twitterStatus = db.twitterStatus.find_one({ "lang": "es", "language_detections": { "$exists": False } })
+            twitterStatus = db.twitterStatus.find_one({ "language_detections": { "$exists": False } })
+            text = twitterStatus['text'].encode('utf-8')
+            result = detectlanguage.detect(text)
 
-            result = detectlanguage.detect(twitterStatus['text'])[0]
+            if len(result) == 0:
+                result = {}
+                result['source'] = 'detectlanguage'
+                twitterStatus['language_detections'] = []
+            else:
+                result = result[0]
+                result['source'] = 'detectlanguage'
 
-            result['source'] = 'detectlanguage'
-
-            twitterStatus['language_detections'] = []
-            twitterStatus['language_detections'].append(result)
+                twitterStatus['language_detections'] = []
+                twitterStatus['language_detections'].append(result)
 
             db.twitterStatus.update( { "_id": twitterStatus['_id']}, twitterStatus, upsert=True)
 
